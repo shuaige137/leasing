821601699d3d309a73befabed66dc89b229a65e0
Danil <dalmatdan@yandex.ru>
2025-12-07 00:45:19 +0300

    Big changes...

--- src/main/java/com/example/leasing_spring/LeasingEntity.java
+++ src/main/java/com/example/leasing_spring/LeasingEntity.java
@@ -7,51 +7,70 @@ import jakarta.persistence.*;
 public class LeasingEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @Column(name = "first_name")
-    private String firstName;
-    @Column(name = "last_name")
-    private String lastName;
-    @Column(name = "number")
-    private String number;
-    @Column(name = "score")
-    private float score;
+    @Column(name = "client_id")
+    private Long clientId;
+    @Column(name = "client_type")
+    private String clientType;
+    @Column(name = "full_name")
+    private String fullName;
+    @Column(name = "agreement_n", unique = true)
+    private String agreementN;
+    @Column(name = "inn")
+    private String inn;
+    @Column(name = "phone")
+    private String phone;
+    @Column(name = "email")
+    private String email;
 
     protected LeasingEntity() {
     }
 
-    public Long getId() {
-        return this.id;
+    public Long getClientId() {
+        return this.clientId;
     }
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return this.firstName;
-    }
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+    public void setClientId(Long clientId) {
+        this.clientId = clientId;
     }
 
-    public String getLastName() {
-        return this.lastName;
+    public String getClientType() {
+        return this.clientType;
     }
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getNumber() {
-        return this.number;
-    }
-    public void setNumber(String number) {
-        this.number = number;
+    public void setClientType(String clientType) {
+        this.clientType = clientType;
     }
 
-    public float getScore() {
-        return this.score;
+    public String getFullName() {
+        return this.fullName;
     }
-    public void setScore(float score) {
-        this.score = score;
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getAgreementN() {
+        return this.agreementN;
+    }
+    public void setAgreementN(String agreementN) {
+        this.agreementN = agreementN;
+    }
+
+    public String getInn() {
+        return this.inn;
+    }
+    public void setInn(String inn) {
+        this.inn = inn;
+    }
+
+    public String getPhone() {
+        return this.phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
--- src/main/java/com/example/leasing_spring/LeasingRepository.java
+++ src/main/java/com/example/leasing_spring/LeasingRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface LeasingRepository extends JpaRepository<LeasingEntity, Long> {
-    @Query("SELECT p FROM LeasingEntity p WHERE CONCAT(p.firstName, ' ', p.lastName, ' ', p.number, ' ', p.score) LIKE %:keyword%")
+    @Query("SELECT p FROM LeasingEntity p WHERE CONCAT(p.clientType, ' ', p.fullName, ' ', p.agreementN, ' ', p.inn, ' ', p.phone, ' ', p.email) LIKE %:keyword%")
     List<LeasingEntity> search(@Param("keyword") String keyword);
 }
